672b7efa5f3a1e218a05c9dc2681273f009fa37c
Yoni-Git <yonatan.goshime@gmail.com>
2025-05-01 14:52:54 -0500

    componet added

--- App.js
+++ App.js
@@ -1,19 +1,61 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-// const header = React.createElement("div", { id: "parent" }, [
-//   React.createElement("div", { id: "child1" }, [
-//     React.createElement("h1", {}, "I am h1"),
-//     React.createElement("h2", {}, "I am h2"),
-//   ]),
 
-//   React.createElement("div", { id: "child2" }, [
-//     React.createElement([
-//       React.createElement("h1", {}, "I am h1"),
-//       React.createElement("h2", {}, "I am h2"),
-//     ]),
-//   ]),
-// ]);
-const header = React.createElement("div", { id: "parent" }, "I am h1");
+/*
+this is food app
+ 
+ header
+    logo 
+    nav-items
+ body
+   resturant card
+footer
+   copy right 
+    links
+
+*/
+    
+const Header = () => {
+    return (<section className='header'>
+        <div className="logo-container">
+            <img src="https://www.shutterstock.com/image-vector/food-logo-smile-label-company-260nw-1271590297.jpg"/>
+        </div>
+        <nav className='nav-container'>
+            <ul>
+                <li>Home</li>
+                <li>About </li>
+                <li>Contact</li>
+                <li>Chart</li>
+            </ul>
+        </nav>
+
+    </section>)
+}
+const ResturantCard = () => {
+   return( <div className='resturnat'>
+            <img className='res-img' alt='pngtree' src='https://png.pngtree.com/template/20200704/ourmid/pngtree-restaurant-logo-design-vector-template-image_388753.jpg' />
+            <h2>png tree rest</h2>
+            <h3>North indian ,Asia, </h3>
+          <h4>4.4 star</h4>
+        <h5>35 mint</h5>
+    </div>)
+}
+const Body = () => {
+    return (
+        <section className='body'>
+            <div className="search">Search</div>
+            <did className='res-container'>
+                <ResturantCard/>
+            </did>
+        </section>
+    )
+}
+const AppLayout = () => {
+    return (<div className="app">
+        <Header />
+        <Body/>
+    </div>)
+}
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(header);
+root.render(<AppLayout/>);
